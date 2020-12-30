da91ac25d2868534a8bf7ac8ba734dd00c204177
陈晓 <yuyedaocao@qq.com>
2020-12-30 15:13:09 +0800

    创建

---
+++ code/src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    redirect: "login",
+	meta: {
+		name: '主页'
+	}
+  },
+  {
+    path: '/login',
+    name: 'Login',
+	meta: {
+		name: '登录'
+	},
+    component: () => import('../views/Login/index.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ code/src/views/Login/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="login">
+		<el-tabs v-model="activeName" @tab-click="handleClick">
+			<el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
+			<el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+		</el-tabs>
+	</div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeName: 'second'
+      };
+    },
+    methods: {
+      handleClick(tab, event) {
+        console.log(tab, event);
+      }
+    }
+  };
+</script>
+
+<style>
+	.login{
+		width: 400px;
+		margin: 0 auto;
+	}
+</style>
